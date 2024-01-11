6ec4a4c096b2c9b4f8b2a408909bf05f6f87627f
LAPTOP-TMT\tranm <tranminhtuan1002@gmail.com>
2024-01-11 17:45:02 +0700

    Update page restrictions

--- src/components/setting/profiles/restricion/constants.js
+++ src/components/setting/profiles/restricion/constants.js
@@ -3,6 +3,35 @@ import {
   CHOOSE_PROFILE_C,
   CHOOSE_PROFILE_U,
 } from "../../../../assets/images/settings/constantImg";
+import styled from "styled-components";
+
+const CheckboxWrapper = styled.div`
+  position: relative;
+  font-size: 14px;
+  margin-bottom: 10px;
+`;
+
+const Checkbox = styled.input`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: 0;
+`;
+
+const CustomCheckbox = styled.div`
+  width: 27px;
+  height: 27px;
+  border: 1px solid #b3b3b3;
+  background-color: #fff;
+  margin-right: 10px;
+  display: inline-block;
+  box-shadow: ${(props) => (props.isClicked ? "0 0 3px #0080ff" : "none")};
+`;
+
+const CustomLabel = styled.label`
+  cursor: default;
+  font-size: 1.2em;
+`;
 
 const listUser = [
   {
@@ -80,4 +109,89 @@ const listManagement = [
   },
 ];
 
-export { listSupports, listUser, listManagement };
+const optionsRestriction = [
+  {
+    title: "Tất cả",
+    tooltip: "Đề xuất cho mọi đối tượng khán giả",
+    value: "35",
+  },
+  {
+    title: "7+",
+    tooltip: "Đề xuất cho độ tuổi từ 7 trở lên",
+    value: "42",
+  },
+  {
+    title: "10+",
+    tooltip: "Đề xuất cho độ tuổi từ 10 trở lên",
+    value: "48",
+  },
+  {
+    title: "13+",
+    tooltip: "Đề xuất cho độ tuổi từ 13 trở lên",
+    value: "54",
+  },
+  {
+    title: "16+",
+    tooltip: "Phù hợp với độ tuổi từ 16 trở lên",
+    value: "60",
+  },
+  {
+    title: "18+",
+    tooltip: "Phù hợp với độ tuổi từ 18 trở lên",
+    value: "66",
+  },
+];
+
+const searchItemResult = [
+  {
+    title: "Phim 1",
+  },
+  {
+    title: "Phim 2",
+  },
+  {
+    title: "Phim 3",
+  },
+  {
+    title: "Phim 4",
+  },
+  {
+    title: "Phim 5",
+  },
+  {
+    title: "Phim 6",
+  },
+  {
+    title: "Phim 7",
+  },
+  {
+    title: "Phim 8.1",
+  },
+  {
+    title: "Phim 8.2",
+  },
+  {
+    title: "Phim 8.3",
+  },
+  {
+    title: "Phim 8.4",
+  },
+  {
+    title: "Phim 8.5",
+  },
+  {
+    title: "Phim 8.6",
+  },
+];
+
+export {
+  listSupports,
+  listUser,
+  listManagement,
+  optionsRestriction,
+  Checkbox,
+  CustomCheckbox,
+  CheckboxWrapper,
+  CustomLabel,
+  searchItemResult,
+};
--- src/components/setting/profiles/restricion/restrictionPwd.js
+++ src/components/setting/profiles/restricion/restrictionPwd.js
@@ -7,43 +7,84 @@ import RestrictionsRank from "./restrictionRank";
 export default function Restrictions() {
   const [restrictionsPwd, setRestrictionsPwd] = useState(true);
   const [restrictionsRank, setRestrictionsRank] = useState(true);
-  
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [errorInput, setErrorInput] = useState("");
+
   const cx = classNames.bind(styles);
 
-  const handleShowRestrictionRank = () => {
-    setRestrictionsPwd(false);
-    setRestrictionsRank(false);
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+    setErrorInput("");
+    setErrorMessage("");
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // if (!password) {
+    //   setErrorInput("#b00500");
+    //   setErrorMessage("Bạn cần nhập mật khẩu.");
+    //   return;
+    // }
+
+    // if (password === "123456") {
+      setRestrictionsPwd(false);
+      setRestrictionsRank(false);
+    // } else {
+    //   setErrorInput("#b00500");
+    //   setErrorMessage("Mật khẩu không chính xác.");
+    //   return;
+    // }
+
+    // setErrorMessage("");
   };
 
   return (
     <>
       {restrictionsPwd && (
-        <div className={cx("detail-content-maxWidth")}>
-          <div className={cx("title-avatar")}>
-            <div className={cx("title")}>
-              <h1>Giới hạn xem</h1>
+        <form onSubmit={handleSubmit}>
+          <div className={cx("detail-content-maxWidth")}>
+            <div className={cx("title-avatar")}>
+              <div className={cx("title")}>
+                <h1>Giới hạn xem</h1>
+              </div>
+              <div className={cx("avatar")}>
+                <img
+                  src={AVATAR_RESTRICTION}
+                  alt="Avatar Profile Restriction"
+                />
+              </div>
             </div>
-            <div className={cx("avatar")}>
-              <img src={AVATAR_RESTRICTION} alt="Avatar Profile Restriction" />
+            <div className={cx("checkPwd")}>
+              <span className={cx("restriction-password")}>
+                Nhập mật khẩu tài khoản để chỉnh sửa Độ tuổi và Giới hạn video
+                cho hồ sơ của Username.
+              </span>
+              <div className={cx("input-checkPwd")}>
+                <input
+                  className={cx("input")}
+                  type="password"
+                  value={password}
+                  onChange={handlePasswordChange}
+                  style={{ borderColor: errorInput }}
+                />
+                <span className={cx("forgetPwd")}>Bạn quên mật khẩu?</span>
+              </div>
+              {errorMessage && (
+                <div className={cx("message")}>{errorMessage}</div>
+              )}
+            </div>
+            <div className={cx("btn-restriction")}>
+              <button type="submit" className={cx("next")}>
+                Tiếp tục
+              </button>
+              <button type="submit" className={cx("cancel")}>
+                Hủy
+              </button>
             </div>
           </div>
-          <div className={cx("checkPwd")}>
-            <span className={cx("restriction-password")}>
-              Nhập mật khẩu tài khoản để chỉnh sửa Độ tuổi và Giới hạn video cho
-              hồ sơ của Username.
-            </span>
-            <div className={cx("input-checkPwd")}>
-              <input className={cx("input")} type="password" />
-              <span className={cx("forgetPwd")}>Bạn quên mật khẩu?</span>
-            </div>
-          </div>
-          <div className={cx("btn-restriction")}>
-            <button className={cx("next")} onClick={handleShowRestrictionRank}>
-              Tiếp tục
-            </button>
-            <button className={cx("cancel")}>Hủy</button>
-          </div>
-        </div>
+        </form>
       )}
       {!restrictionsRank && <RestrictionsRank />}
     </>
