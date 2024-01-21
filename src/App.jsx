cc23978b340f2d4cb8b2305dc14acbf0eda6c768
Phat Huynh <phatht011201@gmail.com>
2024-01-21 11:23:36 +0700

    Refactor App component and remove unused code

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,13 @@ import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import AppRoutes from "./routes/AppRoutes";
-import { useDispatch, useSelector } from "react-redux";
-import { Adding } from "./redux/counterSlice";
 
 const App = () => {
-  const counting = useSelector((e) => e.counter.name);
-  const sending = useDispatch();
   return (
     <>
       <Router>
         <AppRoutes />
       </Router>
-      {/* <p>{counting}</p>
-      <button onClick={()=>sending(Adding())}>Click me</button> */}
     </>
   );
 };
